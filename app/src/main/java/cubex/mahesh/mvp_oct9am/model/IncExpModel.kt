12bc55dace992617bd8eaae145f02f43b35ad122
maheshthippala <mahesh.cubex@gmail.com>
2018-10-28 10:22:55 +0530

    Added

--- app/src/main/java/cubex/mahesh/mvp_oct9am/model/IncExpModel.kt
+++ app/src/main/java/cubex/mahesh/mvp_oct9am/model/IncExpModel.kt
@@ -1,5 +1,6 @@
 package cubex.mahesh.mvp_oct9am.model
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import cubex.mahesh.mvp_oct9am.beans.IncExpBean
@@ -10,20 +11,35 @@ import cubex.mahesh.mvp_oct9am.view.MainActivity
 class IncExpModel : IncExpPresenterAPI
 {
     var dBase : SQLiteDatabase? = null
+    var mActivity:MainActivity? = null
     constructor(api:IncExpViewAPI)
     {
-        var mActivity = api as MainActivity
-        dBase = mActivity.openOrCreateDatabase("incexp_db",
+         mActivity = api as MainActivity
+        dBase = mActivity!!.openOrCreateDatabase("incexp_db",
                 Context.MODE_PRIVATE,null)
         dBase!!.execSQL("create table if not exists incexp(_id integer primary key autoincrement,date varchar(500),money integer,_desc varchar(500),type varchar(100) )")
     }
 
     override fun insertIncExp(bean: IncExpBean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var cv = ContentValues( )
+        cv.put("date",bean.date)
+        cv.put("money",bean.money)
+        cv.put("_desc",bean.desc)
+        cv.put("type",bean.type)
+       var status =  dBase!!.insert("incexp",null, cv)
+        if(status!=-1L)
+        {
+                mActivity!!.insertResponse("Data Insertion Success")
+        }else{
+            mActivity!!.insertResponse("Data Insertion Fail")
+        }
+
     }
 
     override fun readIncExp() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var c = dBase!!.query("incexp",null,null,
+                null,null,null,null)
+        mActivity!!.readResponse(c)
     }
 
 }
--- app/src/main/java/cubex/mahesh/mvp_oct9am/view/MainActivity.kt
+++ app/src/main/java/cubex/mahesh/mvp_oct9am/view/MainActivity.kt
@@ -1,21 +1,82 @@
 package cubex.mahesh.mvp_oct9am.view
 
+import android.app.DatePickerDialog
 import android.database.Cursor
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.SimpleCursorAdapter
+import android.widget.Toast
 import cubex.mahesh.mvp_oct9am.R
+import cubex.mahesh.mvp_oct9am.beans.IncExpBean
+import cubex.mahesh.mvp_oct9am.model.IncExpModel
+import cubex.mahesh.mvp_oct9am.presenter.IncExpPresenterAPI
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity(),IncExpViewAPI {
     override fun insertResponse(msg: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        Toast.makeText(this@MainActivity,
+                msg,Toast.LENGTH_LONG).show()
+      }
 
     override fun readResponse(c: Cursor) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+
+       var cAdapter = SimpleCursorAdapter(this@MainActivity,
+               R.layout.indiview,c,
+               arrayOf("date","money","type","_desc"),
+               intArrayOf(R.id.date,R.id.money,R.id.type,R.id.desc),
+               0)
+        lview.adapter = cAdapter
+
+        var income_sum = 0
+        var expense_sum = 0
+        while(c.moveToNext())
+        {
+            if(c.getString(4).equals("income")){
+                        income_sum = income_sum+ c.getInt(2)
+            }else{
+                expense_sum = expense_sum+ c.getInt(2)
+            }
+        }
+        tv1.text = income_sum.toString()
+        tv2.text = expense_sum.toString()
+   }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        b1.setOnClickListener {
+        var listener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(p0: DatePicker?, p1: Int, p2: Int, p3: Int) {
+                    et1.setText("$p3 - ${p2+1} - $p1")
+            }
+        }
+            var cal = Calendar.getInstance()
+            var year = cal.get(Calendar.YEAR)
+            var month = cal.get(Calendar.MONTH)
+            var day = cal.get(Calendar.DAY_OF_MONTH)
+            // context,OnDateSetListener,year,month,day
+            var dpd = DatePickerDialog(this@MainActivity, listener,
+                    year,month,day)
+            dpd.show()
+
+        }
+        b2.setOnClickListener {
+            var api:IncExpPresenterAPI = IncExpModel(
+                                                                            this@MainActivity)
+            var bean = IncExpBean(et1.text.toString(),
+                                et2.text.toString().toInt(),
+                                et3.text.toString(),sp1.selectedItem.toString())
+            api.insertIncExp(bean)
+
+        }
+        b3.setOnClickListener {
+            var api:IncExpPresenterAPI = IncExpModel(
+                    this@MainActivity)
+            api.readIncExp()
+        }
+
     }
 }
